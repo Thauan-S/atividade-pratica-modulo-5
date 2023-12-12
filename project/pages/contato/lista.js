d48c73e0a9989408aea24e1ebdd4d3d36165b525
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2023-12-12 16:48:39 -0300

    Identação das páginas

--- project/pages/contato/lista.js
+++ project/pages/contato/lista.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Table from '@/components/table'
-import HeadComponent from '@/components/head'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Table from "@/components/table";
+import HeadComponent from "@/components/head";
 
 const Contato = () => {
-    const [contatos,setContatos]=useState(null)
-    useEffect(()=>{
+  const [contatos, setContatos] = useState(null);
+  useEffect(() => {
     axios
-    .get("http://localhost:8080/contatos")
-    .then((response)=>{
-        setContatos(response.data)
-    })
-    .then((error)=>{
-        console.error("erro ao buscar os contatos",error)
-    })
-    },[])
-      return (
-        <div>  
-        <HeadComponent title={"Lista | Contatos"}/>
-         <main>
-         <Table contatos={contatos}/>
-         </main>
-        </div>
-      )
-}
+      .get("http://localhost:8080/contatos")
+      .then((response) => {
+        setContatos(response.data);
+      })
+      .then((error) => {
+        console.error("erro ao buscar os contatos", error);
+      });
+  }, []);
+  return (
+    <div>
+      <HeadComponent title={"Lista | Contatos"} />
+      <main>
+        <Table contatos={contatos} />
+      </main>
+    </div>
+  );
+};
 
-export default Contato
+export default Contato;
--- project/pages/index.js
+++ project/pages/index.js
@@ -1,26 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Card from '@/components/card'
-import Carrousel from '@/components/carrousel'
-import HeadComponent from '@/components/head'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Card from "@/components/card";
+import Carrousel from "@/components/carrousel";
+import HeadComponent from "@/components/head";
 
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   let arrCards = [
     {
-      image:'/img/praia.jpg',
+      image: "/img/praia.jpg",
       title: "Explore o mundo",
       description:
         "De praias paradisíacas a metrópoles vibrantes, nossa agência de viagens está pronta para tornar seus sonhos realidade.Seja qual for o seu desejo de aventura,estamos aqui para fazer cada momento ser memorável.Seja qual for o seu desejo de aventura.",
       buttonText: "Conferir Destinos",
     },
     {
-      image:"/img/venda-com-desconto-especial-em-viagens.jpg",
+      image: "/img/venda-com-desconto-especial-em-viagens.jpg",
       title: " Os melhores preços do mercado",
       description:
         "Oferecemos preços competitivos para tornar suas aventuras acessíveis e inesquecíveis. Com opções para todos os orçamentos, garantimos transparência e excelentes ofertas em passagens, hospedagem e pacotes turísticos.",
@@ -33,38 +31,45 @@ export default function Home() {
         " Na nossa agência de viagens, a segurança é a nossa prioridade número um. Nossos especialistas garantem viagens bem planejadas, parceiros confiáveis e aconselhamento sobre medidas de segurança em todos os destinos.",
     },
     {
-      image:"/img/go.jpg",
+      image: "/img/go.jpg",
       title: "Confiabilidade",
       description:
         " Aqui na nossa agência, colocamos a confiança no centro de tudo o que fazemos. Desde o momento em que você entra em contato conosco, até o dia em que você volta para casa após a viagem dos seus sonhos, estaremos ao seu lado, cuidando de todos os detalhes com diligência e cuidado.",
     },
   ];
   let arrCarrousel = [
-    { title: "Explore locais incriveis", image: "/img/desert.jpg" ,active:"active" },
+    {
+      title: "Explore locais incriveis",
+      image: "/img/desert.jpg",
+      active: "active",
+    },
     { title: "Conheça o mundo", image: "/img/paisagemaaa.jpg" },
-    { title: "Os mais variados locais", image: "/img/pexels-pixabay-279574.jpg" },
+    {
+      title: "Os mais variados locais",
+      image: "/img/pexels-pixabay-279574.jpg",
+    },
   ];
   return (
     <>
-     <HeadComponent title={'Tropical | Início'} />
-     
-        <main className={` container-fluid ${styles.main} ${styles.div}  `}>
-      <div className="row text-center  ">
-        {arrCards.map((i, index) => (
-          <div key={index} className="col-md mb-1">
-            <Card
-              image={i.image}
-              title={i.title}
-              description={i.description}
-              btnText={i.buttonText}
-            />
-          </div>
-        ))}
-      </div>
-      <div className="mt-5 ">
-        <Carrousel  arrCarrousel={arrCarrousel}/>
-      </div>
-      </main>  
+      <HeadComponent title={"Tropical | Início"} />
+
+      <main className={` container-fluid ${styles.main} ${styles.div}  `}>
+        <div className="row text-center  ">
+          {arrCards.map((i, index) => (
+            <div key={index} className="col-md mb-1">
+              <Card
+                image={i.image}
+                title={i.title}
+                description={i.description}
+                btnText={i.buttonText}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="mt-5 ">
+          <Carrousel arrCarrousel={arrCarrousel} />
+        </div>
+      </main>
     </>
-  )
+  );
 }
